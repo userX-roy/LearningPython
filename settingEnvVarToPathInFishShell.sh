2fc2eebcb1310886e5d365587287b199820650fc
userX-roy <saikatroyxyz@gmail.com>
2023-08-26 14:41:15 +0530

    how to set ENV VAR in fish shell

---
+++ settingEnvVarToPathInFishShell.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/fish
+
+###################Adding jar Files in Classpath: ##############################
+
+set -Ux CLASSPATH /home/saikat/Downloads/ImpLibFiles/*.jar
+
+################################################################################
+
+##################---Adding Flutter in Path---##################################
+
+set -Ua fish_user_paths /home/saikat/Documents/FluuterZone/flutter_linux_3.13.0-stable/flutter/bin 
+
